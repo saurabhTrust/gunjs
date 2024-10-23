a2da81c5496626b974081ae5dc8085bf6a73f4c7
Kumar Saurabh <saurabhk@trustgrid.com>
2024-10-23 12:15:01 +0530

    new turn

--- frontend/webrtc.js
+++ frontend/webrtc.js
@@ -1,15 +1,41 @@
+// const iceServers = [
+//   { urls: "stun:stun.relay.metered.ca:80" },
+//   {
+//     urls: "turn:global.relay.metered.ca:80",
+//     username: "71a361258c2adc3899957a54",
+//     credential: "bwZZBXve8wmhiS/A",
+//   },
+//   {
+//     urls: "turn:global.relay.metered.ca:443",
+//     username: "71a361258c2adc3899957a54",
+//     credential: "bwZZBXve8wmhiS/A",
+//   },
+// ];
+
 const iceServers = [
-  { urls: "stun:stun.relay.metered.ca:80" },
+  {
+    urls: "stun:stun.relay.metered.ca:80",
+  },
   {
     urls: "turn:global.relay.metered.ca:80",
-    username: "71a361258c2adc3899957a54",
-    credential: "bwZZBXve8wmhiS/A",
+    username: "32b0819c14efab2f2f28735a",
+    credential: "1ALjeGjsSRk5BuC4",
+  },
+  {
+    urls: "turn:global.relay.metered.ca:80?transport=tcp",
+    username: "32b0819c14efab2f2f28735a",
+    credential: "1ALjeGjsSRk5BuC4",
   },
   {
     urls: "turn:global.relay.metered.ca:443",
-    username: "71a361258c2adc3899957a54",
-    credential: "bwZZBXve8wmhiS/A",
+    username: "32b0819c14efab2f2f28735a",
+    credential: "1ALjeGjsSRk5BuC4",
   },
+  {
+    urls: "turns:global.relay.metered.ca:443?transport=tcp",
+    username: "32b0819c14efab2f2f28735a",
+    credential: "1ALjeGjsSRk5BuC4",
+  }
 ];
 
 class WebRTCHandler {
